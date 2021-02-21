1445934aba3641c1f4dc279459afe42ec3c87815
Jordan King <jordan_king_77@hotmail.co.uk>
2021-02-21 16:12:44 +0000

    Add project files.

---
+++ AnimalAdoptionWebsite_FinalYearProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnimalAdoptionWebsite_FinalYearProject.MvcApplication" Language="C#" %>
---
+++ AnimalAdoptionWebsite_FinalYearProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnimalAdoptionWebsite_FinalYearProject.Startup))]
+namespace AnimalAdoptionWebsite_FinalYearProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
